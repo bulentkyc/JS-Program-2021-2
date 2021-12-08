c39d8fd23f37da17de1c149b2436700435f49f27
BULENT KAYICI <bulent@Bulents-Mini.fritz.box>
2021-12-08 15:21:08 +0100

    v03

--- Loops.js
+++ Loops.js
@@ -10,7 +10,7 @@
  */
 
 var userNumber;
-var randomNumber = Math.ceil(Math.random()*10);
+
 //var counter = 0;
 var diff;
 var preDiff;
@@ -78,54 +78,176 @@ for (let i = 1; i <= 3; i++) {
     alert(i)
 }
  */
+var randomNumber;
 
-for (let counter = 1; counter <= 3; counter++) {
+do {
+    randomNumber = Math.ceil(Math.random()*10);
+    userNumber = prompt(`Would you like to guess my number which is between 1 - 10 with 3 chance.` , `Type only q to quit`);
 
-    //counter++;
-    userNumber = prompt(`Let's guess my number. It's between 1-10 ${randomNumber}` , `Please type only q to quit`);
+    if (userNumber == 'q') {
+        break;
+    }
 
-    diff = Math.abs(randomNumber - userNumber);
-    
-/*     if (preDiff != undefined) {
-        if (diff > preDiff) {
-            temp = 'colder'
+    for (let counter = 1; counter <= 3; counter++) {
+
+        //counter++;
+        userNumber = prompt(`Let's guess my number. It's between 1-10 ${randomNumber}` , `Please type only q to quit`);
+
+        diff = Math.abs(randomNumber - userNumber);
+        
+    /*     if (preDiff != undefined) {
+            if (diff > preDiff) {
+                temp = 'colder'
+            } else {
+                temp = 'warmer'
+            }
         } else {
+            temp = ''
+        } */
+
+        if (preDiff == undefined) {
+            temp = 'no worries this is the first round'
+        } else if (diff > preDiff) {
+            temp = 'colder'
+        } else if (diff < preDiff) {
             temp = 'warmer'
+        } else {
+            temp = 'same'
         }
-    } else {
-        temp = ''
-    } */
 
-    if (preDiff == undefined) {
-        temp = 'no worries this is the first round'
-    } else if (diff > preDiff) {
-        temp = 'colder'
-    } else if (diff < preDiff) {
-        temp = 'warmer'
-    } else {
-        temp = 'same'
-    }
+        preDiff = diff;
 
-    preDiff = diff;
+        if (counter == 3 && userNumber != randomNumber) {
+            alert(`Game is over, you loose`);
+        } else if (userNumber == 'q') {
+            alert(`I'm sorry to hear you don't wanna play with me 😩`);
+            break
+        } else if (userNumber == randomNumber) {
+            alert(`Yeyyy you catch me at ${counter}. time. It was ${randomNumber}`);
+            break
+        } else if (userNumber < randomNumber) {
+            alert(`It's too low and ${temp}`);
+        } else if (userNumber > randomNumber) {
+            alert(`It's too high and ${temp}`);
+        }
 
-    if (counter == 3 && userNumber != randomNumber) {
-        alert(`Game is over, you loose`);
-    } else if (userNumber == 'q') {
-        alert(`I'm sorry to hear you don't wanna play with me 😩`);
-        break
-    } else if (userNumber == randomNumber) {
-        alert(`Yeyyy you catch me at ${counter}. time. It was ${randomNumber}`);
-        break
-    } else if (userNumber < randomNumber) {
-        alert(`It's too low and ${temp}`);
-    } else if (userNumber > randomNumber) {
-        alert(`It's too high and ${temp}`);
-    }
+    } 
 
-} 
+} while ( userNumber != 'q' );
 /* 
 if (userNumber != randomNumber) {
     alert(`Game is over, you loose`);
 } */
 
 //while (userNumber != randomNumber && userNumber != 'q');
+
+var output = '';
+
+for (let i = 0; i < 5; i++) {
+    output += '*';
+    console.log(output);
+}
+
+
+console.log('    *'); // 4 space + 1 star
+console.log('   **'); // 3 space + 2 star
+console.log('  ***'); // 2 space + 3 star
+console.log(' ****'); // 1 space + 4 star
+console.log('*****'); // 0 space + 5 star
+
+
+for (let i = 0; i < 5; i++) {
+    var text = '';
+    for (let c = 0; c < 4-i; c++) {
+        text += ' ';
+    }
+    for (let x = 0; x <= i; x++) {
+        text += '*';
+    }
+    console.log(text);
+}
+
+/* 
+for (let i = 0; i < 5; i++) {
+    var text = '';
+    for (let c = 0; c < 4-i; c++) {
+        text += ' ';
+    }
+    for (let x = 0; x < (i*2)-1; x++) {
+        text += '*';
+    }
+    console.log(text)
+} */
+
+
+console.log('    *'); // 4 space + 1 star
+console.log('   ***'); // 3 space + 3 star
+console.log('  *****'); // 2 space + 5 star
+console.log(' *******'); // 1 space + 7 star
+console.log('*********'); // 0 space + 9 star
+
+
+for (let i = 0; i < 5; i++) {
+    var text = '';
+    for (let c = 0; c < 5-i; c++) {
+        text += ' ';
+    }
+    for (let x = 0; x < i*2+1; x++) {
+        text += '*';
+    }
+    console.log(text);
+}
+/* 
+odd   even
+1       2
+3       4
+5       6   
+7       8
+ */
+/* 
+var txt = "";
+for(i=1;i<=10;i++){
+  txt = "";
+  for(j=i;j<10;j++){
+    txt += " ";
+  }
+  for(k=0;k<(i*2)-1;k++){
+    txt += "*";
+  }
+  console.log(txt);
+} */
+
+
+console.log('    *'); // 4 space + 1 star
+console.log('   ***'); // 3 space + 3 star
+console.log('  *****'); // 2 space + 5 star
+console.log(' *******'); // 1 space + 7 star
+console.log('*********'); // 0 space + 9 star
+console.log(' *******'); // 1 space + 7 star
+console.log('  *****'); // 2 space + 5 star
+console.log('   ***'); // 3 space + 3 star
+console.log('    *'); // 4 space + 1 star
+
+for (let i = 0; i < 5; i++) {
+    var text = '';
+    for (let c = 0; c < 5-i; c++) {
+        text += ' ';
+    }
+    for (let x = 0; x < i*2+1; x++) {
+        text += 'A';
+    }
+
+    console.log(text);
+}
+
+for (let i = 0; i < 5; i++) {
+    var text = '';
+    for (let c = 0; c < i+1; c++) {
+        text += ' ';
+    }
+    for (let x = 0; x < 9-i*2; x++) {
+        text += 'v';
+    }
+
+    console.log(text);
+}
